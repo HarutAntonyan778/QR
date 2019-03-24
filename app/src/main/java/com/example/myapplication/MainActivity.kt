526e7212f0b9e7bd5367ce9f3ab97a296b5c5d9e
Harut Antonyan <hantonyan@beachbody.com>
2019-03-24 17:36:04 +0400

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val animDrawable: AnimationDrawable = anim.background as AnimationDrawable
-        animDrawable.setEnterFadeDuration(2000)
-        animDrawable.setExitFadeDuration(3000)
-        animDrawable.start()
+
+// ...
+        database = FirebaseDatabase.getInstance().getReference("Cafe").push()
+        Log.d("gfdfsd" + database.key.toString(),"fdsfsd")
+//        Toast.makeText(this,"fsdf s" + database.key..toString(),Toast.LENGTH_LONG)
+
+
+//        val animDrawable: AnimationDrawable = anim.background as AnimationDrawable
+//        animDrawable.setEnterFadeDuration(2000)
+//        animDrawable.setExitFadeDuration(3000)
+//        animDrawable.start()
         button.setOnClickListener {
             IntentIntegrator(this).initiateScan()
         }
